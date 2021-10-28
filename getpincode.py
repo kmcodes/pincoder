360c0ced8212f26574e1549a9110f8cb6f8b5101
kmcodes <6884180+kmcodes@users.noreply.github.com6884180+kmcodes@users.noreply.github.com>
2021-10-28 15:13:32 +0530

    first commit

---
+++ getpincode.py
@@ -0,0 +1,45 @@
+# pip install geopy==2.2.0
+import geopy
+import csv
+
+def get_pin_code(lat=None, longitude=None):
+    geolocator = geopy.Nominatim(user_agent="check_1")
+    location = geolocator.reverse((lat, longitude),timeout=None)
+    try:
+        postcodes =  (location.raw['address']['postcode'])
+    except:
+        print(None)
+        postcodes = ""
+    return postcodes
+
+lat=19.07283
+longitude=72.88261
+# print(get_pin_code(lat=19.07283, longitude=72.88261))
+
+output_list = []
+with open('output_csv.csv','w') as out_file:
+    writer = csv.writer(out_file)
+    with open('input_csv.csv','r') as csv_file:
+        csv_reader = csv.reader(csv_file,delimiter=',')
+        line_count = 0
+        row_output=[]
+        for row in csv_reader:            
+            print(line_count)
+            line_output = []
+            if line_count==0:
+                pass
+                line_count+=1
+            else:
+                a = get_pin_code(lat=row[0],longitude=row[1])
+                line_count+=1
+                line_output.append(row[0])
+                line_output.append(row[1])
+                line_output.append(a)
+                row_output.append(line_output)
+        writer.writerows(row_output)
+# print (row_output)
+
+                
+
+
+
